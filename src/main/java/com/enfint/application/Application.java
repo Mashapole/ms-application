c947e41291258db46b6054a3c65c7c571362a86e
Mashapole <ralezemoshake@gmail.com>
2022-11-25 18:51:34 +0200

    pushing first load

---
+++ src/main/java/com/enfint/application/Application.java
@@ -0,0 +1,19 @@
+package com.enfint.application;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+		log.info("----------------------------");
+		log.info("The ms-application has started");
+		log.info("----------------------------");
+	}
+
+}
